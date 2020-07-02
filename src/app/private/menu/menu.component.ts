20c0756a51fa7c750c5005364953432bf5dc270d
LuciaHub <luciaprogramacion11@gmail.com>
2020-07-02 10:16:20 +0200

    menu

---
+++ src/app/private/menu/menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'ps-menu.component',
+    templateUrl: './menu.component.html'
+  })
+  export class MenuComponent {
+   
+  }
+  
---
+++ src/app/private/menu/menu.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { MenuComponent } from './menu.component';
+
+
+@NgModule({
+  declarations: [
+    MenuComponent
+  ],
+  exports: [
+    MenuComponent
+  ]
+})
+export class MenuModule {}
