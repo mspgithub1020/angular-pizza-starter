eeabc462bf12eb6d9826c8380078e54bfc23921e
Isidro Torregrosa <isistorre95@gmail.com>
2020-07-03 10:12:25 +0200

    fix

--- src/app/public/login/login.component.ts
+++ src/app/public/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 import { ErrorsService } from '../../core/validation/errors.service';
 import { LoginService } from './login.service';
 import { Router } from '@angular/router';
@@ -13,49 +13,48 @@ import { AuthService } from 'src/app/core/auth/auth.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-@ViewChild('form') form: FormGroup;
-@ViewChild('email') email: FormControl;
+  @ViewChild('form') form: FormGroup;
 
-model={
-  email: '',
-  password: ''
-};
-errors = {
-  email: {
-    required: 'You must enter an email',
-    email: 'Not a valid email'
-  },
-  password: {
-    required: 'You must enter a password',
-    minlength: 'Your password must be at least 3 characters long'
+  model = {
+    email: '',
+    password: ''
+  };
+  errors = {
+    email: {
+      required: 'You must enter an email',
+      email: 'Not a valid email'
+    },
+    password: {
+      required: 'You must enter a password',
+      minlength: 'Your password must be at least 3 characters long'
+    }
+  };
+
+  loginError = '';
+
+  constructor(
+    public errorsService: ErrorsService,
+    private loginService: LoginService,
+    private router: Router,
+    private authService: AuthService
+  ) { }
+
+  onSubmit() {
+    if (this.form.invalid) {
+      return;
+    }
+    this.loginService.login(this.model)
+      .then(this.onLoginSuccess)
+      .catch(this.onLoginError);
   }
-};
-
-loginError = '';
-
-constructor(
-  public errorsService: ErrorsService,
-  private loginService: LoginService,
-  private router: Router,
-  private authService: AuthService
-) { }
-
-onSubmit() {
-  if (this.form.invalid) {
-    return;
+  private onLoginSuccess = (data: { token: string }) => {
+    this.authService.token = data.token;
+    this.router.navigateByUrl('/', { replaceUrl: true });
   }
-  this.loginService.login(this.model)
-    .then(this.onLoginSuccess)
-    .catch(this.onLoginError);
-}
-private onLoginSuccess = (data: {token: string}) => {
-  this.authService.token = data.token;
-  this.router.navigateByUrl('/', {replaceUrl: true});
-}
-private onLoginError = (res: HttpErrorResponse) => {
-  const error = res.status === 401
-  ? 'Incorrect credentials'
-  : 'Unexpected error';
-  this.loginError = error;
-}
+  private onLoginError = (res: HttpErrorResponse) => {
+    const error = res.status === 401
+      ? 'Incorrect credentials'
+      : 'Unexpected error';
+    this.loginError = error;
+  }
 }
