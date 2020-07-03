1ba1162e029d8ac2cbb717948be350fd3bb68b72
Antonio Peinado <60774350+AntonioPeinado@users.noreply.github.com>
2020-07-03 12:15:41 +0200

    propagar

--- src/app/private/header/header.component.ts
+++ src/app/private/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 
 @Component({
@@ -6,14 +6,10 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-@Output()
-
-export class HeaderComponent  {
-
-    OnInit(){
-
-    }
+export class HeaderComponent {
+  @Output()
+  propagar = new EventEmitter<string>();
   onPropagar() {
-
+    this.propagar.emit();
   }
 }
--- src/app/private/header/header.module.ts
+++ src/app/private/header/header.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { HeaderComponent } from './header.component';
-
+import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, MatToolbarModule],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatIconModule],
   exports: [HeaderComponent],
 })
 export class HeaderModule {}
