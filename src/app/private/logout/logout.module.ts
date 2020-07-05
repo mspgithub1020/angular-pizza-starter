d0b7fe6071469eab31993ab1b429b9b348af96b8
Antonio Peinado <60774350+AntonioPeinado@users.noreply.github.com>
2020-07-05 18:55:59 +0200

    icon

--- src/app/private/logout/logout.module.ts
+++ src/app/private/logout/logout.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core';
-import { LogoutComponent } from './logout.component'
 import { MatButtonModule } from '@angular/material/button';
+import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { from } from 'rxjs';
+import { LogoutComponent } from './logout.component'
+
 @NgModule({
   declarations: [LogoutComponent],
   imports: [MatButtonModule, MatIconModule],
