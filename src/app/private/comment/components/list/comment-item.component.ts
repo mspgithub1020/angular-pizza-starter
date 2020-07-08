0faf14b4552eda0616d2841868b29dcc3f33b445
mspgithub <mspformacion1020@gmail.com>
2020-07-08 02:02:28 +0200

    comment-list

--- src/app/private/comment/components/list/comment-item.component.ts
+++ src/app/private/comment/components/list/comment-item.component.ts
@@ -9,4 +9,24 @@ import {Comment} from '../../comment';
 
 export class CommentItemComponent {
   @Input() comment: Comment;
+
+  getStars(score){
+
+    const color = [];
+    const limit = 10;
+    const dif = limit - score;
+
+    for(var i = 0 ; i < limit ; i++){
+      (i < dif ? color.push('black') : color.push('white'))
+    }
+
+    return color;
+
+  }
+
+
+
+
+
+
 }
--- src/app/private/comment/components/list/comment-list.module.ts
+++ src/app/private/comment/components/list/comment-list.module.ts
@@ -1,32 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CommentListComponent } from './comment-list.component';
-<<<<<<< HEAD
-import { CommentItemModule } from './comment-item.module';
 
-import {MatExpansionModule} from '@angular/material/expansion';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 
-
-=======
 import { CommentItemComponent } from './comment-item.component';
->>>>>>> 6b6c711d166adf17a696f3c6d56009521f7ae352
+
 @NgModule({
   declarations: [
     CommentListComponent,
-    CommentItemComponent
+    CommentItemComponent,     
   ],
   exports: [
     CommentListComponent
   ],
   imports: [
     CommonModule,
-<<<<<<< HEAD
-    CommentItemModule,
-    MatExpansionModule,
+    MatExpansionModule,       
     MatIconModule
-=======
->>>>>>> 6b6c711d166adf17a696f3c6d56009521f7ae352
+    
   ]
 })
 export class CommentListModule { }
