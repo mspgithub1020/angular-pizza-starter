79bfd80e4a5217854a55afcb454c224caf166c02
mspgithub <mspformacion1020@gmail.com>
2020-07-08 12:15:25 +0200

    update commet-list

--- src/app/private/comment/components/list/comment-item.component.ts
+++ src/app/private/comment/components/list/comment-item.component.ts
@@ -1,6 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { Comment } from '../../comment';
 
+const icons = {
+  light: 'star_border',
+  dark: 'star'  
+};
+const classes = {
+  light: 'icon--light',
+  dark: 'icon--dark'
+}
+
 @Component({
   selector: 'ps-comment-item',
   templateUrl: './comment-item.component.html',
@@ -8,7 +17,36 @@ import { Comment } from '../../comment';
 })
 
 export class CommentItemComponent {
+
   @Input() comment: Comment;
+
+  getStars(score) {
+    const color = [];
+    const limit = 10;
+    const dif = limit - score;
+    for (let i = 0; i < limit; i++) {
+      const type = i < dif ? 'dark' : 'light';
+      color.push({
+        icon: icons[type],
+        className: classes[type]
+      });
+    }
+    return color;
+  }
+
+  getIconClasses(color){
+    return 'icon--${color}';
+  }
+  
+  getIcon(color){
+    return color === 'dark' ? 'star' : 'star_border'
+  }
+
+}
+
+
+/*
+MODIFICAMOS LA LOGICA , TRABAJAMOS LA LOGICA DE LAS STARS DESDE LA CLASE
 
   getStars(score) {
     const color = [];
@@ -20,9 +58,15 @@ export class CommentItemComponent {
     return color;
   }
 
+Y EVITAMOS EL USO EN HTML DE:
 
+  <mat-icon *ngFor="let color of getStars(comment.score)" class="icon--{{color}}">
+    <ng-container *ngIf="color==='dark'; else light">
+            star
+    </ng-container>
+    <ng-template #light>
+            star_border
+    </ng-template>
+  </mat-icon>
 
-
-
-
-}
+*/
