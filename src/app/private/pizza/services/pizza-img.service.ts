bb200c07136808d8077d75410170a6b11fe1bb91
Isidro Torregrosa <isistorre95@gmail.com>
2020-07-10 10:21:59 +0200

    correccion

--- src/app/private/pizza/services/pizza-img.service.ts
+++ src/app/private/pizza/services/pizza-img.service.ts
@@ -1,2 +1,11 @@
-// resolver la construccion de urls para las imagenes de las pizzas
-// utilizando el servicio de entorno (core/env)
+import { Injectable, Inject } from '@angular/core';
+import { Pizza } from '../pizza';
+import { ENVIRONMENT, environment } from 'src/app/core/env/env.provider';
+
+@Injectable()
+export class PizzaImgService {
+  constructor(@Inject(ENVIRONMENT) private env: environment) { }
+  calculate(pizza: Pizza) {
+    return this.env.serverURL + pizza.img;
+  }
+}
