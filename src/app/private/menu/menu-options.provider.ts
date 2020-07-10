b4d85eb53a5af00b3c91992e027e25d418c66bc3
Isidro Torregrosa <isistorre95@gmail.com>
2020-07-10 13:04:34 +0200

    abs link

--- src/app/private/menu/menu-options.provider.ts
+++ src/app/private/menu/menu-options.provider.ts
@@ -1,6 +1,6 @@
 const menuOptions = [
-  { link: 'pizzas/list', text: 'Show Pizzas Team One', icon: 'local_pizza' },
-  { link: 'pizzas/add', text: 'Add Pizza', icon: 'add_circle' },
+  { link: '/pizzas/list', text: 'Show Pizzas Team One', icon: 'local_pizza' },
+  { link: '/pizzas/add', text: 'Add Pizza', icon: 'add_circle' },
 ];
 
 export const menuOptionsToken = 'MENU_OPTIONS';
