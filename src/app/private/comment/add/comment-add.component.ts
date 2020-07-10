7942b14e5932fbbbf9bf3d596f5203ed15bc6d71
mspgithub <mspformacion1020@gmail.com>
2020-07-10 10:12:53 +0200

    comment-add

--- src/app/private/comment/add/comment-add.component.ts
+++ src/app/private/comment/add/comment-add.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, Input } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ErrorsService } from '../../../core/validation/errors.service';
 import { CommentService } from '../services/comment-http.service';
@@ -11,6 +11,8 @@ import { CommentService } from '../services/comment-http.service';
 
 export class CommentAddComponent{
   // TODO:input con el id de la pizza
+    @Input() id: string;
+
     @ViewChild('form') private form: NgForm;
 
     model = {
@@ -18,19 +20,33 @@ export class CommentAddComponent{
         text: ''
     };
 
+    errors = {
+        score: {
+            required: 'La puntuación de la pizza es requerida'
+        },
+        text: {
+            required: 'Debe incluir comentarios'
+        },
+    };
+
     constructor(
         public errorsService: ErrorsService,
         private commentService: CommentService,
     ) { }
 
-    onSubmit() {
-        if (this.form.invalid) {
-          return;
-        }
+    createComment(){
         // TODO: mas el id de la pizza
         this.commentService.create(this.model)
           .then(this.onCommentSuccess)
           .catch(this.onCommentError);
+    }
+
+
+    onSubmit() {
+        if (this.form.invalid) {
+          return;
+        }
+        this.createComment();
     }
 
     onCommentSuccess(){
