9aa861d3edacea5a5b0ba6ea3cd3b9c0c16fb328
Isidro Torregrosa <isistorre95@gmail.com>
2020-07-13 09:13:50 +0200

    use img service

--- src/app/private/pizza/pages/detail/pizza-info.component.ts
+++ src/app/private/pizza/pages/detail/pizza-info.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, Inject } from '@angular/core';
-import { ENVIRONMENT, environment } from 'src/app/core/env/env.provider';
+import { Component, Input } from '@angular/core';
+import { PizzaImgService } from '../../services/pizza-img.service';
 
 @Component({
   selector: 'ps-pizza-info',
@@ -8,15 +8,14 @@ import { ENVIRONMENT, environment } from 'src/app/core/env/env.provider';
 })
 export class PizzaInfoComponent {
   @Input() pizza;
-  constructor(@Inject(ENVIRONMENT) private env: environment) {}
+  constructor(private pizzaImgService: PizzaImgService) {}
   getImg() {
-    return this.env.serverURL + this.pizza.img;
+    return this.pizzaImgService.calculate(this.pizza.img);
   }
   shouldPrintDivider(i: number) {
     return i < this.pizza.ingredients.length - 1;
   }
   getPrice() {
-
     return this.pizza.ingredients.reduce((total, ing) => total + ing.prize, 0);
   }
 }
