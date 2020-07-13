c9e07ca329cbc3d83c3c1c031304a2c2f3a11df4
Isidro Torregrosa <isistorre95@gmail.com>
2020-07-13 10:22:33 +0200

    prod server

--- server/config.js
+++ server/config.js
@@ -10,10 +10,18 @@ module.exports = {
     staticPath:  path.resolve(__dirname, '..', 'build'),
     db: path.join(__dirname, 'db.json'),
     statics: {
-        upload: path.resolve(__dirname,'upload')
+        upload: path.resolve(__dirname,'upload'),
+        app: path.resolve(__dirname, '..', 'dist', 'pizza-shop')
     },
     imagesDir: 'images',
-
+    csp: {
+      directives: {
+        defaultSrc: ["'self'"],
+        styleSrc: ["'self'", 'https://fonts.googleapis.com', "'unsafe-inline'"],
+        fontSrc: ["'self'", 'https://fonts.gstatic.com']
+      }
+    },
+    referrerPolicy: 'strict-origin',
     authentication: {
         authSecret: process.env.AUTH_SECRET || 'auth_secret',
         refreshSecret: process.env.REFRESH_SECRET || 'refresh_secret',
--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fallback = require('express-history-api-fallback');
+const helmet = require('helmet');
 const { DBManager } = require('./db-manager');
 const api = require('./api');
 
@@ -9,6 +11,16 @@ async function create(config) {
     Object.values(config.statics).forEach((dir) => {
         app.use(express.static(dir));
     });
+    app.use(
+      helmet.contentSecurityPolicy(config.csp),
+      helmet.referrerPolicy({
+        policy: config.referrerPolicy
+      }),
+      helmet({
+        hidePoweredBy: true
+      }),
+      fallback('index.html', { root: config.statics.app })
+    );
     app.listen(config.port, () => {
         console.log(`server listening on http://localhost:${config.port}`);
     });
